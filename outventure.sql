acc8569d2027ab87f063174b62e53df996221670
KuroP1 <55400967+KuroP1@users.noreply.github.com>
2023-03-02 02:31:29 +0800

    HUGE: update new database
    Since I update new database so all function should not work right now I will do it tommorow

--- outventure.sql
+++ outventure.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 27, 2023 at 11:44 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Host: localhost
+-- Generation Time: Mar 01, 2023 at 07:29 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,98 +24,190 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Table structure for table `Categories`
 --
 
-CREATE TABLE `category` (
-  `category_id` int(255) NOT NULL,
-  `category_name` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='category';
+CREATE TABLE `Categories` (
+  `CategoryID` int(11) NOT NULL,
+  `CategoryName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Categories`
+--
+
+INSERT INTO `Categories` (`CategoryID`, `CategoryName`) VALUES
+(1, 'test');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Table structure for table `FavoriteLists`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `product_name` varchar(255) NOT NULL,
-  `product_des` varchar(255) NOT NULL,
-  `product_size` char(2) DEFAULT NULL,
-  `product_color` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='products table';
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `product_name`, `product_des`, `product_size`, `product_color`) VALUES
-(1, 'cookie', 'jeng shit', 'L', 'red');
+CREATE TABLE `FavoriteLists` (
+  `FavoriteID` int(11) NOT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `Orders`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `Orders` (
+  `OrderID` int(11) NOT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `ProductID` int(11) DEFAULT NULL,
+  `UserID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `users`
+-- Table structure for table `ProductComments`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`) VALUES
-(19, 'asdasda@asda2.com', 'sdadasdasdadasd'),
-(20, '1@1.com', '12345678');
+CREATE TABLE `ProductComments` (
+  `CommentID` int(11) NOT NULL,
+  `CommentContent` varchar(255) DEFAULT NULL,
+  `CommentDate` date DEFAULT NULL,
+  `ProductID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Products`
+--
+
+CREATE TABLE `Products` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(255) DEFAULT NULL,
+  `ProductDescription` varchar(255) DEFAULT NULL,
+  `ProductQuantity` int(11) DEFAULT NULL,
+  `ProductSize` varchar(255) DEFAULT NULL,
+  `ProductColor` varchar(255) DEFAULT NULL,
+  `PositiveVote` int(11) DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Products`
+--
+
+INSERT INTO `Products` (`ProductID`, `ProductName`, `ProductDescription`, `ProductQuantity`, `ProductSize`, `ProductColor`, `PositiveVote`, `CategoryID`) VALUES
+(1, 'Cookie', 'hehe', 5, 'L', 'red', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `isAdmin` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`UserID`, `Username`, `Email`, `Password`, `isAdmin`) VALUES
+(1, 'Shiro', '1@1.com', '12345678', 0),
+(2, 'Kuro', '2@1.com', '12345678', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `category`
+-- Indexes for table `Categories`
 --
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
+ALTER TABLE `Categories`
+  ADD PRIMARY KEY (`CategoryID`);
 
 --
--- Indexes for table `products`
+-- Indexes for table `FavoriteLists`
 --
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `FavoriteLists`
+  ADD PRIMARY KEY (`FavoriteID`),
+  ADD KEY `ProductID` (`ProductID`),
+  ADD KEY `UserID` (`UserID`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `Orders`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `Orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `ProductID` (`ProductID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `ProductComments`
+--
+ALTER TABLE `ProductComments`
+  ADD PRIMARY KEY (`CommentID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `Products`
+--
+ALTER TABLE `Products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `CategoryID` (`CategoryID`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`UserID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT for table `Users`
 --
-ALTER TABLE `category`
-  MODIFY `category_id` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `products`
+-- Constraints for dumped tables
 --
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `users`
+-- Constraints for table `FavoriteLists`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+ALTER TABLE `FavoriteLists`
+  ADD CONSTRAINT `favoritelists_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`),
+  ADD CONSTRAINT `favoritelists_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+
+--
+-- Constraints for table `Orders`
+--
+ALTER TABLE `Orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+
+--
+-- Constraints for table `ProductComments`
+--
+ALTER TABLE `ProductComments`
+  ADD CONSTRAINT `productcomments_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`);
+
+--
+-- Constraints for table `Products`
+--
+ALTER TABLE `Products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `Categories` (`CategoryID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
