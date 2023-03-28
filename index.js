14c141bae35cee98b2f0143d0997cefc122aa97e
tonywong586 <72515867+tonywong586@users.noreply.github.com>
2023-03-28 21:38:08 +0800

    update

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ function toggleActive() {
   //get top-menu through id
   const topMenu = document.getElementById("top-menu");
   const topSection = document.getElementById("top-section");
-
+  const burgerBtn = document.getElementById("burger-btn");
+  burgerBtn.classList.toggle("active");
   //set top-menu to height of 100vh
   if (!isOpen) {
     //get height of top-section
     height = topSection.offsetHeight;
     topMenu.style.height = height + "px";
     isOpen = true;
+    topMenu.style.transitionDuration = "0.5s";
   } else if (isOpen) {
     topMenu.style.height = "0%";
     isOpen = false;
@@ -22,9 +24,11 @@ function toggleActive() {
       console.log("resize");
       height = topSection.offsetHeight;
       topMenu.style.height = height + "px";
+      topMenu.style.transitionDuration = "0s";
     }
     //if windows size is greater than 768px, set top-menu to height of 0
-    if (window.innerWidth > 768) {
+    if (window.innerWidth > 992) {
+      burgerBtn.classList.toggle("active");
       isOpen = false;
       topMenu.style.height = "0%";
     }
