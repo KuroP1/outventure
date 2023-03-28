eb7613fff28eabbea1119d935193c9a606065fd4
tonywong586 <72515867+tonywong586@users.noreply.github.com>
2023-03-28 19:15:24 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 var isOpen = false;
 function toggleActive() {
   //get burger btn through id
-  const burger = document.getElementById("burger-btn");
   //get top-menu through id
   const topMenu = document.getElementById("top-menu");
+  const topSection = document.getElementById("top-section");
+
   //set top-menu to height of 100vh
   if (!isOpen) {
-    topMenu.style.height = "100%";
+    //get height of top-section
+    height = topSection.offsetHeight;
+    topMenu.style.height = height + "px";
     isOpen = true;
   } else if (isOpen) {
     topMenu.style.height = "0%";
     isOpen = false;
   }
+
+  //if resized window, set top-menu to height of 0
+  window.addEventListener("resize", function () {
+    if (isOpen) {
+      console.log("resize");
+      height = topSection.offsetHeight;
+      topMenu.style.height = height + "px";
+    }
+    //if windows size is greater than 768px, set top-menu to height of 0
+    if (window.innerWidth > 768) {
+      isOpen = false;
+      topMenu.style.height = "0%";
+    }
+  });
 }
