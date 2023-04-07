ca144cf09c44a9e3e2f1b5e619a445fdb5fd4ce9
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-04-08 00:30:39 +0800

    Multiple Product Image

--- deleteProduct.php
+++ deleteProduct.php
@@ -3,23 +3,28 @@ require_once 'config/database.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('config/database.php');
-function deleteProduct($product_id, $conn)
+function deleteProduct($product_name, $conn)
 {
-    $deleteProductSQL = "DELETE FROM Products WHERE ProductID = ?";
+    $deleteProductSQL = "DELETE FROM Products WHERE ProductName = ?";
     $stmt = $conn->prepare($deleteProductSQL);
-    $stmt->bind_param('i', $product_id);
+    $stmt->bind_param('s', $product_name);
     $stmt->execute();
+
+    $deleteProductSQL2 = "DELETE FROM Images WHERE ProductName = ?";
+    $stmt2 = $conn->prepare($deleteProductSQL2);
+    $stmt2->bind_param('s', $product_name);
+    $stmt2->execute();
 }
 
-if (isset($_GET['id']) && !empty($_GET['id'])) {
-    $product_id = $_GET['id'];
-    deleteProduct($product_id, $conn);
+if (isset($_GET['name']) && !empty($_GET['name'])) {
+    $product_name = $_GET['name'];
+    deleteProduct($product_name, $conn);
 
     // Redirect back to the admin dashboard or another page
     header("Location: admin_dashboard.php");
     exit();
 } else {
-    error_log("Error: Invalid product ID.");
+    error_log("Error: Invalid product name.");
     exit();
 }
 ?>
