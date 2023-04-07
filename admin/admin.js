1685b85ecf906d7d9059122df096dc82bfe1d340
tonywong586 <72515867+tonywong586@users.noreply.github.com>
2023-04-07 23:25:35 +0800

    update admin panel

---
+++ admin/admin.js
@@ -0,0 +1,36 @@
+var isOpen = false;
+function toggleActive() {
+  //get burger btn through id
+  //get top-menu through id
+  const topMenu = document.getElementById("top-menu");
+  const topSection = document.getElementById("top-section");
+  const burgerBtn = document.getElementById("burger-btn");
+  burgerBtn.classList.toggle("active");
+  if (!isOpen) {
+    //get height of top-section
+    height = topSection.offsetHeight;
+    topMenu.style.height = height + "px";
+    isOpen = true;
+    topMenu.style.zIndex = "50";
+    topMenu.style.transitionDuration = "0.5s";
+  } else if (isOpen) {
+    topMenu.style.height = "0%";
+    isOpen = false;
+  }
+
+  //if resized window, set top-menu to height of 0
+  window.addEventListener("resize", function () {
+    if (isOpen) {
+      console.log("resize");
+      height = topSection.offsetHeight;
+      topMenu.style.height = height + "px";
+      topMenu.style.transitionDuration = "0s";
+    }
+    //if windows size is greater than 768px, set top-menu to height of 0
+    if (window.innerWidth > 992) {
+      burgerBtn.classList.toggle("active");
+      isOpen = false;
+      topMenu.style.height = "0%";
+    }
+  });
+}
