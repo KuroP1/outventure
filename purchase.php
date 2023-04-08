24c13b19b41d2960af89f4644be5623335474624
KuroP1 <55400967+KuroP1@users.noreply.github.com>
2023-04-08 23:41:33 +0800

    rand
    rand

--- purchase.php
+++ purchase.php
@@ -17,29 +17,57 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $productName = $cartRow['ProductName'];
             $buyQuantity = intval($cartRow['BuyQuantity']);
             $productPrice = floatval($cartRow['ProductPrice']);
-            $orderID = '1';
+            $orderID = rand(0, 999999);
             $paymentMethod = 'Cash';
+            //check order id is unique
+            $sqlCheck = "SELECT * FROM Orders WHERE OrderID = $orderID";
+            $resultCheck = mysqli_query($conn, $sqlCheck);
+            if (mysqli_num_rows($resultCheck) > 0) {
+                $orderID = rand(999999);
+                // Update the product quantity in the products table
+                $sqlUpdate = "UPDATE products SET ProductQuantity = ProductQuantity - $buyQuantity WHERE ProductName = '$productName'";
+                mysqli_query($conn, $sqlUpdate);
 
-            // Update the product quantity in the products table
-            $sqlUpdate = "UPDATE products SET ProductQuantity = ProductQuantity - $buyQuantity WHERE ProductName = '$productName'";
-            mysqli_query($conn, $sqlUpdate);
+                // Calculate the total amount
+                $amount = $productPrice * $buyQuantity;
 
-            // Calculate the total amount
-            $amount = $productPrice * $buyQuantity;
+                // Get the current date and time
+                $currentDate = date('Y-m-d');
 
-            // Get the current date and time
-            $currentDate = date('Y-m-d');
+                // Insert the order into the Orders table
+                $sqlInsert = "INSERT INTO Orders (OrderDate, OrderID, paymentMethod, orderStatus, Username, Amount, ProductName, BuyQuantity ) VALUES ('$currentDate', '$orderID', '$paymentMethod', '$orderStatus', '$username', $amount, '$productName', $buyQuantity)";
+                mysqli_query($conn, $sqlInsert);
 
-            // Insert the order into the Orders table
-            $sqlInsert = "INSERT INTO Orders (OrderDate, OrderID, paymentMethod, orderStatus, Username, Amount, ProductName, BuyQuantity ) VALUES ('$currentDate', '$orderID', '$paymentMethod', '$orderStatus', '$username', $amount, '$productName', $buyQuantity)";
-            mysqli_query($conn, $sqlInsert);
+                // Remove item from the cart
+                $sqlDelete = "DELETE FROM cart WHERE CartID = $cartID";
+                mysqli_query($conn, $sqlDelete);
+            } else {
+                // Update the product quantity in the products table
+                $sqlUpdate = "UPDATE products SET ProductQuantity = ProductQuantity - $buyQuantity WHERE ProductName = '$productName'";
+                mysqli_query($conn, $sqlUpdate);
 
-            // Remove item from the cart
-            $sqlDelete = "DELETE FROM cart WHERE CartID = $cartID";
-            mysqli_query($conn, $sqlDelete);
+                // Calculate the total amount
+                $amount = $productPrice * $buyQuantity;
+
+                // Get the current date and time
+                $currentDate = date('Y-m-d');
+
+                // Insert the order into the Orders table
+                $sqlInsert = "INSERT INTO Orders (OrderDate, OrderID, paymentMethod, orderStatus, Username, Amount, ProductName, BuyQuantity ) VALUES ('$currentDate', '$orderID', '$paymentMethod', '$orderStatus', '$username', $amount, '$productName', $buyQuantity)";
+                mysqli_query($conn, $sqlInsert);
+
+                // Remove item from the cart
+                $sqlDelete = "DELETE FROM cart WHERE CartID = $cartID";
+                mysqli_query($conn, $sqlDelete);
+            }
+
+
         }
         //add up all the total amounts  and display them where orderID= 1
-        $sql = "SELECT SUM(Amount) FROM Orders WHERE OrderID = 1";
+
+        //select sum(amount) from orders where orderid = $orderID
+        $sql = "SELECT SUM(Amount) FROM Orders WHERE OrderID = $orderID";
+
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_assoc($result);
         $total = $row['SUM(Amount)'];
