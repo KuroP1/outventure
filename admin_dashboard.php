68e45c6ac11611c505f315596a4e91b829e47e44
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-04-08 17:03:25 +0800

    Update admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -80,18 +80,18 @@
 
     <h2>All</h2>
     <?php
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-    require("config/database.php");
-    $viewSQL = "SELECT * FROM images ORDER BY ImageID";
-    $res = mysqli_query($conn, $viewSQL);
-    if (mysqli_num_rows($res) > 0) {
-        while ($images = mysqli_fetch_assoc($res)) {
-            if ($images["ProductName"] == $_GET["name"]) {
-                echo "<img src='" . $images['ImagePath'] . "' width='100px' height='100px'>";
-            }
-        }
-    }
+    // ini_set('display_errors', 1);
+    // error_reporting(E_ALL);
+    // require("config/database.php");
+    // $viewSQL = "SELECT * FROM images ORDER BY ImageID";
+    // $res = mysqli_query($conn, $viewSQL);
+    // if (mysqli_num_rows($res) > 0) {
+    //     while ($images = mysqli_fetch_assoc($res)) {
+    //         if ($images["ProductName"] == $_GET["name"]) {
+    //             echo "<img src='" . $images['ImagePath'] . "' width='100px' height='100px'>";
+    //         }
+    //     }
+    // }
     ?>
 
     <?php
