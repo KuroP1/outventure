cb81f39baccca7fb66c472752036a01fc27bb5fb
Elvin Chu <elvincth@gmail.com>
2023-04-14 16:50:19 +0800

    f

--- config/database.php
+++ config/database.php
@@ -1,17 +1,17 @@
 <?php
-$servername = "localhost";
-$database = "Outventure";
-$username = "root";
-$password = "";
+$servername = "outventure.chfgk5zp2zgw.ap-northeast-1.rds.amazonaws.com";
+$database = "outventure";
+$username = "admin";
+$password = "WH#vCFxKDNGqj9*N";
 
 // Create connection
 
 $conn = mysqli_connect($servername, $username, $password, $database);
+
+
 
 // Check connection
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
-?>
